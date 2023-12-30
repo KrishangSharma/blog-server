fd64b0b8e98c6e2f4c675e581c5fce51e5b833b5
Krishang Sharma <krishang.sharma.17704@gmail.com>
2023-12-30 23:53:15 +0530

    Let's just hope it works

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const port = process.env.PORT || 3000;
 // Create instance of express
 const app = express();
 app.use(cors());
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://blog-application-kr0s.onrender.com"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 // Enable routes
 app.use(blogRoutes);
