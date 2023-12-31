5062f73416be36ace5ae88e528b2360584f850bb
Krishang Sharma <krishang.sharma.17704@gmail.com>
2023-12-31 13:53:47 +0530

    CORS Issues lvl 1

--- controller/blogController.js
+++ controller/blogController.js
@@ -6,11 +6,9 @@ const uploadBlog = async (req, res) => {
   try {
     // Check if 'images' field is present in the request
     if (!req.files || req.files.length === 0) {
-      return res
-        .status(100)
-        .json({
-          message: "No files uploaded, proceeding with blof uploading!",
-        });
+      return res.status(100).json({
+        message: "No files uploaded, proceeding with blof uploading!",
+      });
     }
 
     const imageUploadPromises = req.files.map(async (file) => {
