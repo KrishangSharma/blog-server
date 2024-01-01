af586649ebc00eca56bee137da6ff37ad8acdaa9
Krishang Sharma <krishang.sharma.17704@gmail.com>
2024-01-01 22:35:58 +0530

    PLEASE WORK!!!!!

--- index.js
+++ index.js
@@ -10,13 +10,18 @@ const port = process.env.PORT || 3000;
 // Create instance of express
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers":
+      "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+  });
+
+  next();
+});
+
+app.use(cors());
 
 // Start the Server
 app.listen(port, () => {
