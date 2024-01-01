33b29299f356d104361c5fa487b5d4488c888dc1
Krishang Sharma <krishang.sharma.17704@gmail.com>
2024-01-01 23:46:36 +0530

    Updated CORS Config

--- index.js
+++ index.js
@@ -10,25 +10,14 @@ const port = process.env.PORT || 3000;
 // Create instance of express
 const app = express();
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   res.setHeader("Access-Control-Allow-Private-Network", true);
-
-//   next();
-// });
-
 app.use(
   cors({
-    origin: "https://blog-application-ufvo.onrender.com",
+    origin: "*",
+    // origin: ["https://blog-application-ufvo.onrender.com", "http://localhost:5173"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE",
+    allowedHeaders:
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers, api-key ",
+    credentials: true,
   })
 );
 
