ba31273f76a264940e4032f804c72a5388f536c1
Krishang Sharma <krishang.sharma.17704@gmail.com>
2024-01-01 23:59:24 +0530

    hoja

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://blog-application-ufvo.onrender.com"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
