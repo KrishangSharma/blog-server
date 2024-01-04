24ae5113646210bcef3ed737fbe08b3f13c70976
Krishang Sharma <krishang.sharma.17704@gmail.com>
2024-01-04 22:48:55 +0530

    Just checking

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://blog-application-ufvo.onrender.com"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
